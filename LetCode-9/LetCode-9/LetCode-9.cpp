e1a301550811c68d51821d5355c12606fd4e93fe
Anikalogika4417 <anikalogika4417@gmail.com>
2023-11-15 18:57:34 +0200

    first view

--- LetCode-9/LetCode-9/LetCode-9.cpp
+++ LetCode-9/LetCode-9/LetCode-9.cpp
@@ -2,26 +2,32 @@
 //
 
 #include <iostream>
+#include <string>
+
+using namespace std;
+
 
 class Solution {
 public:
     bool isPalindrome(int x) {
-
+        bool in_middle = true;
+        in_middle = to_string(x).size() % 2 == 1 ? true : false;
+        if (in_middle) {
+            if (to_string(x).substr(0, to_string(x).size() / 2 -1) == to_string(x).substr(to_string(x).size() / 2 + 1, to_string(x).size()).reserve()) {
+                return true;
+            }
+        }
+        else {
+            if (to_string(x).substr(0, to_string(x).size() / 2 - 1) == to_string(x).substr(to_string(x).size() / 2, to_string(x).size())) {
+                return true;
+            }
+        }
+        return false;
     }
 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Solution test;
+    cout << test.isPalindrome(131);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
